6d4a994b29bca6478fa96b4ddeb4119cdd78dbfa
asmaljcelj <aljaz.smaljcelj7@gmail.com>
2022-12-29 23:28:45 +0100

    [#33] finished configuring logging
    - also included logging of application name, version and request id

--- src/main/java/com/rso/microservice/filter/LoggingFilter.java
+++ src/main/java/com/rso/microservice/filter/LoggingFilter.java
@@ -1,8 +1,9 @@
 package com.rso.microservice.filter;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.rso.microservice.util.Constants;
 import org.slf4j.MDC;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -10,26 +11,32 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.UUID;
 
 
 @Component
 @Order(1)
 public class LoggingFilter implements Filter {
-    private static final Logger log = LoggerFactory.getLogger(LoggingFilter.class);
 
+    @Autowired
+    BuildProperties buildProperties;
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest req = (HttpServletRequest) servletRequest;
         HttpServletResponse res = (HttpServletResponse) servletResponse;
-        MDC.put("test", "this is a test");
-        log.info(
-                "Logging Request  {} : {}", req.getMethod(),
-                req.getRequestURI());
+
+        String requestId = req.getHeader("X-Request-Id");
+        if (requestId == null)
+            requestId = UUID.randomUUID().toString();
+
+        MDC.put(Constants.MDC_NAME, "Administration");
+        MDC.put(Constants.MDC_REQUEST_ID, requestId);
+        MDC.put(Constants.MDC_VERSION, buildProperties.getVersion());
         filterChain.doFilter(servletRequest, servletResponse);
-        log.info(
-                "Logging Response :{}",
-                res.getContentType());
+        MDC.remove(Constants.MDC_NAME);
+        MDC.remove(Constants.MDC_REQUEST_ID);
+        MDC.remove(Constants.MDC_VERSION);
     }
 
 }
